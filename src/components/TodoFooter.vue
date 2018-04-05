350bb0fd0fad0b8810bce33571605fb460ffbc34
wj-soft <wooju_lee@daum.net>
2018-04-06 01:14:48 +0900

    add & remove

--- src/components/TodoFooter.vue
+++ src/components/TodoFooter.vue
@@ -1,14 +1,33 @@
 <template>
-  <div>fotter</div>
+  <div class="clearAllContainer">
+     <span class="clearAllBtn" v-on:click="clearTodo">Clear All</span>
+  </div>
 </template>
 
 <script>
 export default {
+    methods: {
+        clearTodo() {
+            localStorage.clear();
+        }
+    }
   
 }
 </script>
 
 <style>
+    .clearAllContainer {
+        width: 8.5rem;
+        height: 50px;
+        line-height: 50px;
+        background: white;
+        border-radius: 5px;
+        margin: 0 auto;
+    }
+    .clearAllBtn {
+        color: #e20303;
+        display: block;
+    }
 
 </style>
 
