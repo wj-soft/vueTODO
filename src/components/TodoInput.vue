023a25e73dd724058e051aa31f1e713a9cba29da
wj-soft <wooju_lee@daum.net>
2018-04-07 21:25:22 +0900

    moda

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -4,14 +4,24 @@
     <span class="addContainer" v-on:click="addTodo">
       <i class="addBtn fas fa-plus-square"></i>
     </span>
+
+    <modal v-if="showModal" @close="showModel = false">
+      <h3 slot="header">경고</h3>
+      <span slot="footer" @click="showModal = false">
+        할 일을 입력하세요 <i class="closeModalBtn fa fa-times"></i>
+      </span>
+    </modal>
   </div>
 </template>
 
 <script>
+import Modal from './common/Modal.vue'
+
 export default {
   data(){
     return {
-      newTodoItem: ''
+      newTodoItem: '',
+      showModal: false
     }
   },
   props: ['propsdata'],
@@ -21,11 +31,16 @@ export default {
         var value = this.newTodoItem && this.newTodoItem.trim();
 				this.$emit('addTodo', value)
         this.clearInput();
-      }  
+      } else {
+        this.showModal = !this.showModal;
+      } 
     },
     clearInput(){
       this.newTodoItem = "";
     }
+  },
+  components: {
+    Modal: Modal
   }
 }
 </script>
